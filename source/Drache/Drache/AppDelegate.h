1ed5c2685fa6fe35df6fdf840de87674a0494b5e
Inferis <spam@inferis.org>
2012-07-13 14:27:11 +0200

    Initial checkin.

---
+++ source/Drache/Drache/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Drache
+//
+//  Created by Tom Adriaenssen on 13/07/12.
+//  Copyright (c) 2012 Tom Adriaenssen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ source/Drache/Drache/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Drache
+//
+//  Created by Tom Adriaenssen on 13/07/12.
+//  Copyright (c) 2012 Tom Adriaenssen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
