74b10f4482b3f2a580204671d71cb945c85064b1
Inferis <spam@inferis.org>
2012-07-17 22:40:58 +0200

    Graph now animates.

--- source/Drache/Drache/GraphLayer.m
+++ source/Drache/Drache/GraphLayer.m
@@ -16,6 +16,7 @@
     NSArray* _points;
     CAShapeLayer* _borderLayer;
     CAGradientLayer* _gradientLayer;
+    CAShapeLayer* _mask;
 }
 
 - (id)init {
@@ -34,6 +35,10 @@
         _gradientLayer.colors = [NSArray arrayWithObjects:(id)[UIColor colorWithHex:0x9980ccff].CGColor, [UIColor colorWithHex:0x991e4c67].CGColor, nil];
         [self addSublayer:_gradientLayer];
         
+        _mask = [[CAShapeLayer alloc] init];
+        _gradientLayer.mask = _mask;
+        
+        [self generateMaskAnimated:NO];
     }
     return self;
 }
@@ -55,30 +60,52 @@
     
     UIBezierPath* path = [UIBezierPath bezierPath];
     [path moveToPoint:(CGPoint) { 0, height }];
-    //[path addLineToPoint:(CGPoint) { 0, height-bottom } ];
 
-    CGFloat x = 0;
     CGFloat minY = height-bottom;
-    for (RainPoint* point in _points) {
-        CGFloat y = ((100 - MAX(point.adjustedIntensity, 5)) / 100.0) * (height-bottom);
-        minY = MIN(minY, y);
-        [path addLineToPoint:(CGPoint) { x, y }];
-        x += width/(_points.count-2);
+    CGFloat x = 0;
+    if (_points) {
+        for (RainPoint* point in _points) {
+            CGFloat y = ((100 - MAX(point.adjustedIntensity, 7)) / 100.0) * (height-bottom);
+            minY = MIN(minY, y);
+            [path addLineToPoint:(CGPoint) { x, y }];
+            x += width/(_points.count-2);
+        }
+    }
+    else {
+        for (int i=0; i<7; ++i) {
+            [path addLineToPoint:(CGPoint) { x, height }];
+            x += width/5;
+        }
     }
 
-    //[path addLineToPoint:(CGPoint) { width, height-bottom } ];
     [path addLineToPoint:(CGPoint) { width, height } ];
     [path closePath];
     
-    CAShapeLayer* mask = [[CAShapeLayer alloc] init];
-    mask.frame = self.bounds;
-    mask.path = path.CGPath;
+
+    if (animated) {
+        CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"path"];
+        animation.duration = 0.3;
+        animation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+        animation.fromValue = (id)_mask.path;
+        animation.toValue = (id)path.CGPath;
+        [_mask addAnimation:animation forKey:@"animatePath"];
+    }
+    _mask.frame = self.bounds;
+    _mask.path = path.CGPath;
+
     _gradientLayer.frame = self.bounds;
-    _gradientLayer.mask = mask;
     _gradientLayer.startPoint = (CGPoint) { 0, minY/(height-bottom) };
     _gradientLayer.endPoint = (CGPoint) { 0, 1 };
     
     _borderLayer.frame = self.bounds;
+    if (animated) {
+        CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"path"];
+        animation.duration = 0.3;
+        animation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+        animation.fromValue = (id)_borderLayer.path;
+        animation.toValue = (id)path.CGPath;
+        [_borderLayer addAnimation:animation forKey:@"animatePath"];
+    }
     _borderLayer.path = path.CGPath;
 }
 
