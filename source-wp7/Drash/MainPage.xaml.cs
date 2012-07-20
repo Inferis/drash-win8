acbab01b036add2d51f696af768705ae217cd5b9
Tom Adriaenssen <tom@inferis.org>
2012-07-20 11:13:13 +0200

    Implemented rain fetching and parsing.

--- source-wp7/Drash/MainPage.xaml.cs
+++ source-wp7/Drash/MainPage.xaml.cs
@@ -1,24 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Device.Location;
 using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 
 namespace Drash
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        private GeoCoordinateWatcher watcher;
+        private bool fetchingRain;
+        private RainData rain;
+
         // Constructor
         public MainPage()
         {
             InitializeComponent();
+
+            watcher = new GeoCoordinateWatcher();
+            watcher.MovementThreshold = 500;
+            watcher.PositionChanged += (sender, args) => {
+                FetchRain();
+            };
+            watcher.Start();
+        }
+
+        private void FetchRain()
+        {
+            if (fetchingRain) return;
+
+            fetchingRain = true;
+            var uri = string.Format("http://gps.buienradar.nl/getrr.php?lat={0}&lon={1}", watcher.Position.Location.Latitude, watcher.Position.Location.Longitude);
+
+            var wc = new WebClient();
+            wc.DownloadStringCompleted += (sender, args) => {
+                RainData.TryParse(args.Result, out rain);
+                fetchingRain = false;
+            };
+            wc.DownloadStringAsync(new Uri(uri));
         }
     }
 }
---
+++ source-wp7/Drash/RainPoint.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Globalization;
+
+namespace Drash
+{
+    internal class RainPoint
+    {
+        private RainPoint(DateTime stamp, int value)
+        {
+            Stamp = stamp;
+            Value = (int)(value * 100.0 / 255.0);
+            Precipitation = value == 0 ? 0 : Math.Pow(10, (value - 109.0) / 32.0);
+            AdjustedValue = (int)(Math.Min(Value, 70) / 70.0 * 100.0);
+            var logisticIntensity = (Value - 14) / 40.0 * 12.0;
+            Intensity = (int)Math.Round(1 / (1 + Math.Pow(Math.E, -logisticIntensity)) * 100);
+        }
+
+        public DateTime Stamp { get; private set; }
+        public int Value { get; private set; }
+        public int AdjustedValue { get; private set; }
+        public int Intensity { get; private set; }
+        public double Precipitation { get; private set; }
+
+        public static RainPoint Parse(string source)
+        {
+            var parts = source.Trim().Split(new[] { '|' });
+
+            var stamp = DateTime.ParseExact(parts[1], "HH:mm", CultureInfo.InvariantCulture);
+            if (stamp.Subtract(DateTime.Now).TotalHours < -2)
+                stamp = stamp.AddDays(1);
+
+            return new RainPoint(stamp, int.Parse(parts[0]));
+        }
+    }
+}
