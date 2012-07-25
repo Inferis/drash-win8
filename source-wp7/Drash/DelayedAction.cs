ed47876036e74394040139bfdb99b67a029f7264
Tom Adriaenssen <tom@inferis.org>
2012-07-25 15:49:51 +0200

    DelayedAction no longer blocks the thread it is called from and always calls back on main thread.

--- source-wp7/Drash/DelayedAction.cs
+++ source-wp7/Drash/DelayedAction.cs
@@ -1,18 +1,28 @@
 using System;
 using System.Threading;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace Drash
 {
     internal class DelayedAction : IDisposable
     {
+        private readonly Dispatcher dispatcher;
         private ManualResetEvent waiter = new ManualResetEvent(false);
+
+        public DelayedAction(Dispatcher dispatcher)
+        {
+            this.dispatcher = dispatcher;
+        }
 
         public void Run(Action action, int delay)
         {
             Cancel();
-            var canceled = waiter.WaitOne(delay);
-            if (!canceled)
-                action();
+            ThreadPool.QueueUserWorkItem(o => {
+                var canceled = waiter.WaitOne(delay);
+                if (!canceled)
+                    dispatcher.BeginInvoke(action);
+            });
         }
 
         public void Cancel()
