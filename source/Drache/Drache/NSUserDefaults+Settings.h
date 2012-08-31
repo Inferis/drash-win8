4b559767091fe8120e319f87e91552607a114d06
Inferis <spam@inferis.org>
2012-08-31 12:00:06 +0200

    Added settings category on NSUserDefaults.

---
+++ source/Drache/Drache/NSUserDefaults+Settings.h
@@ -0,0 +1,15 @@
+//
+//  NSStandardUserDefaults+Settings.h
+//  Drache
+//
+//  Created by Tom Adriaenssen on 31/08/12.
+//  Copyright (c) 2012 Tom Adriaenssen. All rights reserved.
+//
+
+
+
+@interface NSUserDefaults (Settings)
+
+@property (nonatomic, assign) int entries;
+
+@end
