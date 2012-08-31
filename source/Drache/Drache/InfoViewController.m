ffa0c6c29f7208b6fdb2c9e56f4633e2d5e86594
Inferis <spam@inferis.org>
2012-08-31 12:02:52 +0200

    Changed layout of infoviewcontroller.

--- source/Drache/Drache/InfoViewController.m
+++ source/Drache/Drache/InfoViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "InfoViewController.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface InfoViewController ()
 
@@ -28,14 +29,25 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.contentSizeForViewInPopover = self.view.bounds.size;
+    
     self.acknowledgmentsTextView.text = @"Free weather data:\r\n  http://gratisweerdata.buienradar.nl/\r\nCoby by @junkiesxl:\r\n  http://github.com/pjaspers/coby\r\nTin by @junkiesxl:\r\n  http://github.com/pjaspers/tin\r\nAFNetworking by Github:\r\n  http://github.com/AFNetworking/AFNetworking/";
 }
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-    dispatch_delayed(0.15, ^{
-        [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleDefault animated:YES];
-    });
+    
+    if (!IsIPad()) {
+        dispatch_delayed(0.15, ^{
+            [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleDefault animated:YES];
+        });
+    }
+    else {
+        UIView* popview = [[[self.view superview] superview] superview];
+        [popview.subviews objectAtIndex:1];
+        CALayer* layer = popview.layer;
+        layer.shadowColor = [[UIColor whiteColor] CGColor];
+    }
 }
 
 - (IBAction)closeTapped:(id)sender {
