cf24351787706dc15279b5aaa0621a9a45f7402b
Tom Adriaenssen <tom.adriaenssen@interfaceimplementation.be>
2012-11-25 16:32:24 +0100

    Added background task. Non functional.

---
+++ source-win8/Drash/Drash/BackgroundTask.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Windows.ApplicationModel.Background;
+
+namespace Drash
+{
+    class BackgroundTask : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+        }
+    }
+}
